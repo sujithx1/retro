8310efd96debf3b525c9df71986e99ff1c7b7c1e
sujith <syjithkpy@gmail.com>
2024-06-14 19:57:46 +0530

    changed url

--- helpers/oAuth.js
+++ helpers/oAuth.js
@@ -28,7 +28,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({ 
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"http://localhost:3000/auth/google/callback",
+    callbackURL:"http://www.retroshoponline.shop/auth/google/callback",
     // profileFields:['id', 'displayName', 'name', 'gender', 'picture.type(large)','email'],
     passReqToCallback: true
 

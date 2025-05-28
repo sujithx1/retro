bf1111b5de045110cccae5d18d9b166f13d5494a
sujith <syjithkpy@gmail.com>
2025-05-28 15:54:25 +0530

    added client url

--- helpers/oAuth.js
+++ helpers/oAuth.js
@@ -28,7 +28,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({ 
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"http://:3000/auth/google/callback",
+    callbackURL:`${process.env.CLIENT_URL}/auth/google/callback`,
     // profileFields:['id', 'displayName', 'name', 'gender', 'picture.type(large)','email'],
     passReqToCallback: true
 
